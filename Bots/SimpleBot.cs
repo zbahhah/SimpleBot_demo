79ce0b8d99748a41f90f577dc0f441f31e277cd1
zbahhah <eng.zaky@gmail.com>
2018-10-19 23:40:28 +0300

    Add project files.

---
+++ Bots/SimpleBot.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace demo1.Bots
+{
+    public class SimpleBot : IBot
+    {
+        public async Task OnTurnAsync(ITurnContext turnContext, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            if(turnContext.Activity.Type is ActivityTypes.Message)
+            {
+                var input = turnContext.Activity.Text;
+                await turnContext.SendActivityAsync($"SimpleBot: {input}");
+            }
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,65 @@
+﻿
+using demo1.Bots;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Bot.Builder.BotFramework;
+using Microsoft.Bot.Builder.Integration.AspNet.Core;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace demo1
+{
+    public class Startup
+    {
+        public string ContentRootPath { get; set; }
+
+        //public Startup(IConfiguration configuration)
+        public Startup(IHostingEnvironment env)
+        {
+            //Configuration = configuration;
+            ContentRootPath = env.ContentRootPath;
+        }
+
+        //public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(ContentRootPath)
+                .AddJsonFile("appsettings.json")
+                .AddEnvironmentVariables();
+
+            var configuration = builder.Build();
+
+            services.AddSingleton(configuration);
+
+            // add your simple bot to your app
+            services.AddBot<SimpleBot>(options =>
+            {
+                options.CredentialProvider = new ConfigurationCredentialProvider(configuration);
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseStaticFiles();
+
+            app.UseBotFramework();
+
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
+            //else
+            //{
+            //    app.UseHsts();
+            //}
+
+            //app.UseHttpsRedirection();
+            //app.UseMvc();
+        }
+    }
+}
