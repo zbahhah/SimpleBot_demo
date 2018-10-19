21c62bd0415fff18c6b53ae1bf8db3bb6b2036d8
zbahhah <eng.zaky@gmail.com>
2018-10-20 02:08:37 +0300

    add demo2

---
+++ Demo2SimpleMiddleware/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Demo2SimpleMiddleware
+{
+    using Microsoft.AspNetCore;
+    using Microsoft.AspNetCore.Hosting;
+
+    public class Program
+    {
+        /// <summary>
+        /// Mains the specified arguments.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        /// <created>10/20/2018</created>
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        /// <summary>
+        /// Creates the web host builder.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        /// <returns></returns>
+        /// <created>10/20/2018</created>
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
